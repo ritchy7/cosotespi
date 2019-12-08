f9b2e5bb696d207bbbd535488c9216eb4d1135a4
mnrdev <mnrdev@macbookpro2018ritchy.home>
2019-12-08 01:50:37 +0100

    add(create_database.sh): echo

--- create_database.sh
+++ create_database.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # Create user.
+echo "################################################################"
+echo "######################## Create user ###########################"
+echo "################################################################"
 sudo -u postgres psql -c "CREATE ROLE vagrant LOGIN PASSWORD '1234';"
 
+echo "################################################################"
+echo "################## Grant user to superuser #####################"
+echo "################################################################"
 # Grant the user to SuperUser to install UUID module.
 sudo -u postgres psql -c "ALTER USER vagrant WITH SUPERUSER;"
 
 
+echo "################################################################"
+echo "###################### Create database #########################"
+echo "################################################################"
 # Create database.
 sudo -u postgres psql -c "
     CREATE DATABASE
@@ -15,11 +24,20 @@ sudo -u postgres psql -c "
     OWNER=vagrant;
 "
 
+echo "################################################################"
+echo "####################### Create tables ##########################"
+echo "################################################################"
 # Create tables.
 PGPASSWORD=1234 psql -U vagrant restaurant < create_table_bdd.sql
 
+echo "################################################################"
+echo "################## Remove to user superuser ####################"
+echo "################################################################"
 # Remove the superuser mode.
 sudo -u postgres psql -c "ALTER USER vagrant WITH NOSUPERUSER;"
 
+echo "################################################################"
+echo "################### Connecting to database #####################"
+echo "################################################################"
 # Connect to the database.
 PGPASSWORD=1234 psql -U vagrant restaurant
